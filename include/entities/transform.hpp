5dac4f4814738c0adbb52b81f104250ce70efba4
Juan Pablo Casillas Zermeño <36060613+ZerJuanPablo@users.noreply.github.com>
2025-02-07 16:48:15 +0100

    Revert "Created Instancing branch"
    This reverts commit 0d29f85b29bc9db39fdfebc5d7f3f7e51e0c6aad.

--- include/entities/transform.hpp
+++ include/entities/transform.hpp
@@ -22,19 +22,6 @@ struct Transform {
         glUniformMatrix4fv(4, 1, false, glm::value_ptr(normal_matrix));
     }
 
-    glm::mat4 get_matrix() const {
-        glm::mat4x4 transform_matrix(1.0);
-        glm::mat4x4 normal_matrix(1.0);
-        // calculate transform/model matrix from transform components
-        transform_matrix = glm::translate(transform_matrix, _position);
-        normal_matrix = glm::rotate(normal_matrix, _rotation.x, glm::vec3(1, 0, 0));
-        normal_matrix = glm::rotate(normal_matrix, _rotation.y, glm::vec3(0, 1, 0));
-        normal_matrix = glm::rotate(normal_matrix, _rotation.z, glm::vec3(0, 0, 1));
-        transform_matrix = transform_matrix * normal_matrix;
-        transform_matrix = glm::scale(transform_matrix, _scale);
-        return transform_matrix;
-    }
-
     glm::vec3 _position = glm::vec3(0, 0, 0);
     glm::vec3 _rotation = glm::vec3(0, 0, 0); // euler angles
     glm::vec3 _scale = glm::vec3(1, 1, 1);
